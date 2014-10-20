4526ada3f7e7db40f47c293f3fcde438b500ed71
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-10-20 21:09:53 +0800

    add new and update show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,5 +7,20 @@ class GroupsController < ApplicationController
      @group = Group.find(params[:id])
    end
    
+   def new
+     @group = Group.new
+   end
+
+   def create  
+     @group = Group.new(params[:group])
+     if @group.save 
+        redirect_to groups_path
+     else
+ 	render :new
+     end 	
+
+   end 
+  
+
    
 end

2d1b6ab1310e9ca7c155e3eb20da39374a37e33c
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-10-20 21:30:50 +0800

    add edit  & update page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,46 @@
-class GroupsController < ApplicationController 
-   def index
-   	@groups = Group.all
-   end
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
 
-   def show
-     @group = Group.find(params[:id])
-   end
-   
-   def new
-     @group = Group.new
-   end
+  def show
+    @group = Group.find(params[:id])
+  end
 
-   def create  
-     @group = Group.new(params[:group])
-     if @group.save 
-        redirect_to groups_path
-     else
- 	render :new
-     end 	
+  def new
+    @group = Group.new
+  end
 
-   end 
-  
+  def create
+    @group = Group.new(params[:group])
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
 
-   
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+
+  end
+
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+    def group_params
+      params.require(:group).permit(:title, :description)
+    end
+
 end
